d78a5d0f56a77ae8fe25ca2e97c52dbbf2863cb0
Jacek Migdal <jacek@sumologic.com>
2013-05-13 15:12:16 -0700

    Argument tuning.

--- src/main/scala/pl/migdal/jacek/scala/hystrix/DistributedApplication.scala
+++ src/main/scala/pl/migdal/jacek/scala/hystrix/DistributedApplication.scala
@@ -1,10 +1,10 @@
 package pl.migdal.jacek.scala.hystrix
 
 class DistributedApplication {
-  val Auth = new WebService(42L, 200, 100, 0.01, 0.01, 0.01)
-  val Billing = new WebService(43L, 200, 100, 0.01, 0.01, 0.01)
-  val Monitoring = new WebService(44L, 200, 100, 0.01, 0.01, 0.01)
-  val ContentA = new WebService(45L, 200, 100, 0.01, 0.01, 0.01)
-  val ContentB = new WebService(46L, 200, 100, 0.01, 0.01, 0.01)
+  val Auth = new WebService(42L, 20, 10, 0.01, 0.01, 0.01)
+  val Billing = new WebService(43L, 20, 10, 0.01, 0.01, 0.01)
+  val Monitoring = new WebService(44L, 20, 10, 0.01, 0.01, 0.01)
+  val ContentA = new WebService(45L, 20, 10, 0.01, 0.01, 0.01)
+  val ContentB = new WebService(46L, 20, 10, 0.01, 0.01, 0.01)
 
 }
